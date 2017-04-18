ce46cdb9beba1d6350e487a6110cc0dfb24abf62
accel194 <fujiikiyoyuki@fujiikiyoyuki-no-MacBook-Air.local>
2017-04-18 17:43:44 +0900

    login画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :image, :profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :profile])
   end
 end
