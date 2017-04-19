5898f6a80a0eff344ad86044e5c9f305732976b1
accel194 <fujiikiyoyuki@fujiikiyoyuki-no-MacBook-Air.local>
2017-04-19 16:31:44 +0900

    delete

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
-  has_many :replies, class_name: 'Tweet', foreign_key: 'reply_tweet_id'
+  has_many :replies, class_name: 'Tweet', foreign_key: 'reply_tweet_id', dependent: :destroy
   validates :content, presence: true, allow_blank: false
   scope :not_reply, -> do
     where(reply_tweet_id: nil)
